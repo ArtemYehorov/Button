6223ba64caa0265c7745a80e8bbdfd9dead523d3
Artem <102017036+ArtemYehorov@users.noreply.github.com>
2022-11-28 14:45:28 +0100

    Добавьте файлы проекта.

---
+++ Button/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Button
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int Counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Counter++;
+            button1.Content = Counter;
+        }
+    }
+}
